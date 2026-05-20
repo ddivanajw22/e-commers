90ae0d60cc407183bd67964947b4e5f196992ba7
ivvanaj <d.najwa225@gmail.com>
2026-05-20 20:22:44 +0700

    bismillah

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,11 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -27,22 +25,14 @@ return new class extends Migration
             $table->string('photo')->nullable();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id('id_category');
+            $table->string('nama_kategori');
+            $table->timestamps();
         });
 
         Schema::create('products', function (Blueprint $table) {
+
             $table->id('id_product');
             $table->foreignId('id_seller')->constrained('users');
             $table->foreignId('id_category')->constrained('categories');
@@ -53,14 +43,8 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id('id_category');
-            $table->string('nama_kategori');
-            $table->timestamps();
-        });
-
         Schema::create('orders', function (Blueprint $table) {
-            $table->id('id_order');
+           $table->id('id_order');
             $table->foreignId('id_user')->constrained('users');
             $table->decimal('total_harga',12,2);
             $table->enum('status_order',
@@ -74,22 +58,23 @@ return new class extends Migration
         });
 
         Schema::create('order_details', function (Blueprint $table) {
+
             $table->id('id_detail');
             $table->foreignId('id_order')->constrained('orders');
             $table->foreignId('id_product')->constrained('products');
             $table->integer('qty');
             $table->decimal('subtotal',12,2);
             $table->timestamps();
-        });
 
-        
-//     /**
-//      * Reverse the migrations.
-//      */
-//     // public function down(): void
-//     {
-//         Schema::dropIfExists('users');
-//         Schema::dropIfExists('password_reset_tokens');
-//         Schema::dropIfExists('sessions');
-//     }
-    };
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('users');
+    }
+};
