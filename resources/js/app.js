11b4c0aadf74cac8dee8467c69d4fd04a7c1a64c
miaimup <miaaulia5070@gmail.com>
2026-05-25 15:06:35 +0700

    update lagi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper(".heroSwiper", {
+        loop: true,
+        speed: 1000,          
+        effect: "fade",           
+        fadeEffect: { 
+            crossFade: true 
+        },
+        autoplay: {
+            delay: 3500,            
+            disableOnInteraction: false, 
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
 @section('content')
     <div class="bg-white min-h-screen">
     
