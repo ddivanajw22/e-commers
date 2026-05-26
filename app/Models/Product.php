dab1b2f5d0c82f29cacbf167914ff7367f9098c6
ivvanaj <d.najwa225@gmail.com>
2026-05-26 19:12:43 +0700

    tes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'products';
+    protected $primaryKey = 'id_product';
+
+    protected $fillable = [
+        'id_seller',
+        'id_category',
+        'nama_produk',
+        'deskripsi',
+        'harga',
+        'gambar'
+    ];
 }
