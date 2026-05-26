1c7a924f86b2606a02beb9141739ef211f73e174
miaimup <miaaulia5070@gmail.com>
2026-05-26 19:30:05 +0700

    bismillah

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,8 +3,6 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-
 @section('content')
     <div class="bg-white min-h-screen">
     
