0d2f81cf56ada35aa22a2c5412cf4007ca99029d
miaimup <miaaulia5070@gmail.com>
2026-05-29 15:43:26 +0700

    Update register.blade.php

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -10,36 +10,56 @@
     <div style="max-width: 400px; margin: 50px auto;">
         <h2>Create an Account</h2>
 
-        <form action="{{ route('register.perform') }}" method="POST">
+        <form action="{{ route('register.perform') }}" method="POST" class="space-y-4 max-w-[300px] mx-auto w-full">
             @csrf
 
             <div>
+                <div class="relative">
+                    <span class="absolute inset-y-0 left-3 flex items-center text-gray-400">
+                        <i class="fa-regular fa-user"></i>
+                    </span>
                 <label for="name">Full Name:</label>
                 <input type="text" name="name" id="name" value="{{ old('name') }}" required>
+                    class="w-full pl-10 pr-4 py-2 border border-black rounded-full focus:outline-none"
                 @error('name') <p style="color: red;">{{ $message }}</p> @enderror
             </div>
 
             <br>
 
             <div>
+                <div class="relative">
+                    <span class="absolute inset-y-0 left-3 flex items-center text-gray-400">
+                        <i class="fa-regular fa-envelope"></i>
+                    </span>
                 <label for="email">Email Address:</label>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+                    class="w-full pl-10 pr-10 py-2 border border-black rounded-full focus:outline-none">
                 @error('email') <p style="color: red;">{{ $message }}</p> @enderror
             </div>
 
             <br>
 
             <div>
+                <div class="relative">
+                    <span class="absolute inset-y-0 left-3 flex items-center text-gray-400">
+                        <i class="fa-solid fa-lock"></i>
+                    </span>
                 <label for="password">Password (min 8 chars):</label>
                 <input type="password" name="password" id="password" required>
+                    class="w-full pl-10 pr-10 py-2 border border-black rounded-full focus:outline-none">
                 @error('password') <p style="color: red;">{{ $message }}</p> @enderror
             </div>
 
             <br>
 
             <div>
+                <div class="relative">
+                    <span class="absolute inset-y-0 left-3 flex items-center text-gray-400">
+                        <i class="fa-solid fa-lock"></i>
+                    </span>
                 <label for="password_confirmation">Confirm Password:</label>
                 <input type="password" name="password_confirmation" id="password_confirmation" required>
+                    class="w-full pl-10 pr-10 py-2 border border-black rounded-full focus:outline-none">
             </div>
 
             <br>
